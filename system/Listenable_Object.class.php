e6ba96a0093ae8b143978ed58c815b43f0e94d44
viki53 <viki53000@gmail.com>
2014-05-31 14:21:08 +0200

    Listenable_Object::__toString
    Retourne une chaîne JSON des propriétés de l’objet.

--- system/Listenable_Object.class.php
+++ system/Listenable_Object.class.php
@@ -4,13 +4,13 @@ class Listenable_Object{
 	private properties = array();
 	private events_listeners = array();
 
-	public function __construct($props = array()) {
-		$this->properties = $props;
+	public function __construct($properties = array()) {
+		$this->properties = $properties;
 	}
 
 	public function set($key, $val) {
 		$old_value = isset($this->properties[$key]) ? $this->properties[$key] : NULL;
-		
+
 		$this->properties[$key] = $val;
 
 		$this->trigger('set', array('property' => $key, 'old_value' => $old_value, 'new_value' => $val));
@@ -68,5 +68,9 @@ class Listenable_Object{
 		}
 		
 		return $this;
+	}
+
+	public function __toString() {
+		return json_encode($this->properties);
 	}
 }
