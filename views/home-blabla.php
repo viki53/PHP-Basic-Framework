053c2faf07d8ddc3456f2b1cc7e7e753bc179a1d
viki53 <viki53000@gmail.com>
2014-06-02 02:35:03 +0200

    Controller method parameters in URL example

--- views/home-blabla.php
+++ views/home-blabla.php
@@ -12,15 +12,15 @@
 			echo '<p>Created : '.$obj.'</p>';
 			$obj->saveToFile('obj.json');
 		}
-		if(!empty($obj) && !empty($str)) {
-			echo '<hr />';
-		}
+		
+		echo '<hr />';
+		
 		echo '<h2>Controller parameters (via the URL)</h2>';
 		if(!empty($str)) {
 			echo '<p>You said: '.$str.'</p>';
 		}
 		else {
-			echo '<p>You can change the URL to see what happens. <a href="'.$this->PHPBF->config->get('site_url').'">Try it!</a></p>';
+			echo '<p>You can change the URL to see what happens. <a href="'.$this->PHPBF->config->get('site_url').'/home/blabla/foo_bar">Try it!</a></p>';
 		}
 		?>
 	</body>
